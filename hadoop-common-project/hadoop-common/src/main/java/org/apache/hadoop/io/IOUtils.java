8df1eeb7fd82ff209800e5f1f1810313459d727e
Anshuman <anshuman.a@myntra.com>
2024-06-28 10:49:17 +0530

    Added logger in IOUtils

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/io/IOUtils.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/io/IOUtils.java
@@ -18,6 +18,7 @@
 
 package org.apache.hadoop.io;
 
+
 import java.io.*;
 import java.net.Socket;
 import java.nio.ByteBuffer;
@@ -31,12 +32,12 @@ import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.logging.Log;
-
 import org.apache.hadoop.classification.InterfaceAudience;
 import org.apache.hadoop.classification.InterfaceStability;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.util.Shell;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * An utility class for I/O related functionality. 
